bddd175110101dce18e08c9d95574c04a0199393
WinHtut <winhtutonline@gmail.com>
2022-01-09 00:01:41 +0630

    Create selectionSort.cpp
    adding selection sort

---
+++ selectionSort.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            
+            if (arr[j] < arr[min]) {
+                min = j;   
+            }
+        }
+
+        swap(arr, min, i);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+int main(void)
+{
+    int arr[] = { 3, 5, 8, 4, 1, 9, -2 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
